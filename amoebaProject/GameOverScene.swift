7794a66963d71fe49ef78bec7fe38501c2d46e49
Paulo Ricardo Ramos da Rosa <ricardo.paulorr@gmail.com>
2015-07-01 16:07:40 -0300

    game over fixed

--- amoebaProject/GameOverScene.swift
+++ amoebaProject/GameOverScene.swift
@@ -29,13 +29,13 @@ class GameOverScene: SKScene {
         label.fontSize = 40
         label.fontColor = SKColor.blackColor()
         label.position = CGPoint(x: size.width/2, y: size.height/2)
-        self.addChild(label)
+        //self.addChild(label)
         
         let backgroundImg = SKSpriteNode(imageNamed: "Fundo_Ipad")
         backgroundImg.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
         backgroundImg.size = CGSize(width: self.frame.size.width, height: self.frame.size.height)
         
-        let gameOverImg = SKSpriteNode(imageNamed: "gameoverBack")
+        let gameOverImg = SKSpriteNode(imageNamed: "Game Over")
         gameOverImg.position = CGPointMake(self.frame.size.width/2, self.frame.size.height * 0.7)
         
         
