12de99dd3b0603b638f3d7d5ac37ec99528c7392
Camilla Schmidt <cstefanischmidt@hotmail.com>
2015-07-01 13:18:08 -0300

    como jogar ok

--- amoebaProject/GameOverScene.swift
+++ amoebaProject/GameOverScene.swift
@@ -11,6 +11,8 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
+    var againButton: SKNode! = nil
+    
     override init(size: CGSize) {
         
         super.init(size: size)
@@ -37,25 +39,48 @@ class GameOverScene: SKScene {
         gameOverImg.position = CGPointMake(self.frame.size.width/2, self.frame.size.height * 0.7)
         //backgroundImg.size = CGSize(width: self.frame.size.width, height: self.frame.size.height)
         
+        againButton = SKSpriteNode(imageNamed:"againButton")
+        againButton.position = CGPoint(x:CGRectGetMidX(self.frame), y:self.frame.size.height/2);
+        
         self.addChild(backgroundImg)
         
         self.addChild(gameOverImg)
         
+        self.addChild(againButton)
+    
+        
         // 4
-        runAction(SKAction.sequence([
-            SKAction.waitForDuration(3.0),
-            SKAction.runBlock() {
-                // 5
-                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
-                let scene = GameScene(size: size)
-                self.view?.presentScene(scene, transition:reveal)
-            }
-            ]))
+        //runAction(SKAction.sequence([
+        //    SKAction.waitForDuration(3.0),
+        //    SKAction.runBlock() {
+        //        // 5
+        //        let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+        //        let scene = GameScene(size: size)
+        //        self.view?.presentScene(scene, transition:reveal)
+        //    }
+        //    ]))
         
     }
     
     // 6
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+        // Loop over all the touches in this event
+        for touch: AnyObject in touches
+        {
+            // Get the location of the touch in this scene
+            let location = touch.locationInNode(self)
+            // Check if the location of the touch is within the button's bounds
+            if againButton.containsPoint(location)
+            {
+                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+        }
     }
 }
